ae32744dafca5ab3a0cd9c47ef1212d5aa06121d
joseporres <jose.porres@utec.edu.pe>
2023-07-05 17:28:13 -0500

    feat: menu integration and health icon

--- Assets/Scripts/HpCounter.cs
+++ Assets/Scripts/HpCounter.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 
 public class HpCounter : MonoBehaviour
 {
     protected GameObject textObject;
     protected TextMeshPro textMeshPro;
     public int currentHpCounter;
+    public Sprite sprite;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,7 @@ public class HpCounter : MonoBehaviour
         textObject = new GameObject();
         textMeshPro = textObject.AddComponent<TextMeshPro>();
         textMeshPro.text = currentHpCounter.ToString();
-        textMeshPro.fontSize = 2f;
+        textMeshPro.fontSize = 1.3f;
         textMeshPro.alignment = TextAlignmentOptions.Center;
         textMeshPro.color = Color.white;
 
@@ -29,8 +32,36 @@ public class HpCounter : MonoBehaviour
         canvasObj.AddComponent<RectTransform>();
         textMeshPro.transform.SetParent(canvas.transform);
         RectTransform rectTransform = textMeshPro.GetComponent<RectTransform>();
-        rectTransform.localPosition = new Vector3(0.918f,-0.85f, 1f);
+        rectTransform.localPosition = new Vector3(-39.588f,46.959f, 0.6600075f);
         rectTransform.localRotation = new Quaternion(0, 0, 0, 0);
+        //anchor
+        rectTransform.anchorMin = new Vector2(1, 0);
+        rectTransform.anchorMax = new Vector2(1, 0);
+        //pivot
+        rectTransform.pivot = new Vector2(1, 0);
+
+        //Add Image next to text (to the left)
+        GameObject imageObj = new GameObject("Image");
+        imageObj.transform.SetParent(canvas.transform);
+        imageObj.transform.localPosition = new Vector3(-39.588f, 46.959f, 0.6600075f);
+        imageObj.transform.localRotation = new Quaternion(0, 0, 0, 0);
+        imageObj.transform.localScale = Vector3.one;
+        Image image = imageObj.AddComponent<Image>();
+        image.sprite = sprite;
+        image.color = Color.white;
+        RectTransform imageRectTransform = imageObj.GetComponent<RectTransform>();
+        imageRectTransform.localPosition = new Vector3(-49.866f, 49.71f, 0.314f);
+        imageRectTransform.localRotation = new Quaternion(0, 0, 0, 0);
+
+        //anchor
+        imageRectTransform.anchorMin = new Vector2(1, 0);
+        imageRectTransform.anchorMax = new Vector2(1, 0);
+        //pivot
+        imageRectTransform.pivot = new Vector2(1, 0);
+        //size
+        imageRectTransform.sizeDelta = new Vector2(0.05f,0.05f);
+
+        
         
     }
 
