a61535e77811580398ad90cd6321a266d7b1f920
José Adrián Porres Brugué <jose.porres@utec.edu.pe>
2023-07-11 22:07:19 -0500

    feat: bear death fixed

--- Assets/Scripts/AnimalLife.cs
+++ Assets/Scripts/AnimalLife.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AnimalLife : ObjectLife
 {
-
+    CubeScript cubeScript;
+    NavMeshAgent nav;
+    bool isDead = false; // Flag to track if the object is already dead
 
     public override void TakeDamage(int damage)
     {
-        Animator animator= GetComponent<Animator>();
+        if (isDead) // Check if the object is already dead
+            return;
+
+        Animator animator = GetComponent<Animator>();
         currentHp -= damage;
         if (currentHp <= 0)
         {
             animator.SetBool("Death", true); // Set the bool parameter in the Animator
-            Destroy(gameObject, 15f);
-//            DisableMovement();
+            animator.SetBool("WalkForward",false);
+            Destroy(gameObject, 30f);
+            DisableMovement();
+            isDead = true; // Flag the object as dead
         }
     }
 
     private void DisableMovement()
     {
-        CubeScript cubeScript = GetComponent<CubeScript>();
+        cubeScript = GetComponent<CubeScript>();
         cubeScript.enabled = false; // Disable the CubeScript component to stop movement
-    
+        nav = GetComponent<NavMeshAgent>();
+        nav.enabled = false;
     }
 }
